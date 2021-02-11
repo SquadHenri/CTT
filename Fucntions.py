c7ac8d8690a13f0b4fbae4b9d5cc04cf7b34eee1
Hein <Hein.rodel@gmail.com>
2021-02-11 10:48:29 +0100

    Added 'get distance' for basic distance calc

--- Fucntions.py
+++ Fucntions.py
@@ -1,3 +1,24 @@
-
+import math
 
 def main():
+    print('test')
+
+
+
+def get_distance(container_lcoation, wagon_location):
+    Xdist = 3
+    Ydist = 7
+
+    xContainer = container_lcoation[1]
+    yContainer = container_lcoation[2]
+
+    xWagon = wagon_location[1]
+    yWagon = wagon_location[2]
+
+    xdiff = xContainer - xWagon
+    ydiff = yContainer - yWagon
+
+    x = math.sqrt(math.pow(xdiff, 2) + math.pow(ydiff, 2))
+
+    return x
+
