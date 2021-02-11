3b0bee6df0857e566c633a3c68e9a4314f4a697b
maurits <info@mauritsbecks.com>
2021-02-11 16:02:57 +0100

    Container changed getters

--- scripts/model/Container.py
+++ scripts/model/Container.py
@@ -1,13 +1,27 @@
 class Container():
-    def __init__(self, containerID, weight, length, contents, position):
+    def __init__(self, containerID, weight, teu, position, goods, priority):
         self.containerID = containerID
         self.weight = weight
-        self.length = length
-        self.contents = contents
+        self.teu = teu
         self.position = position
+        self.goods = goods
+        self.priority = priority
 
+    #Getter for container position coordinates
+    def get_containerID(self):
+        return self.containerID
 
-#Getter for container position coordinates
+    def get_weight(self):
+        return self.weight
+
+    def get_teu(self):
+        return self.teu
+
+    def get_goods(self):
+        return self.goods
+
     def get_position(self):
-        return self.position
+        return self.goods
 
+    def get_priority(self):
+        return self.priority
