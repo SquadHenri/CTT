09c7ee7d033c6c88e61d13167c99b57c40633799
dmaneschijn <d.g.j.c.maneschijn@student.utwente.nl>
2021-02-16 17:11:11 +0100

    fixed container and wagon list and the location of wagons

--- scripts/getContainersFromCSV.py
+++ scripts/getContainersFromCSV.py
@@ -4,37 +4,54 @@ import math
 from model.Container import Container
 from model.Wagon import Wagon
 
+# creates a list of container objects. It contains all the containers of train 1.
 def get_containers_1():
     containers = []
+
+    # open file
     with open("data/trein1/loading_container.csv", encoding="utf-8-sig") as container_file:
         data = csv.reader(container_file, delimiter=",")
         for row in data:
+            # initialize variables
             c_id = row[0]
             c_teu = int(row[1])
             c_type = row[2]
-            gross_weight = row[3]
-            net_weight = row[4]
+            gross_weight = float(row[3].replace(".", "").replace(",", "."))
+            net_weight = float(row[4])
             c_pos = get_pos(row[5])
             c_good = row[7]
+            # create object
             container = Container(c_id, gross_weight, net_weight, c_teu * 20, c_pos, c_good, 1, c_type)
+            # add object to container list
             containers.append(container)
     return containers
 
+# creates a list of wagon objects. It contains all the wagons of train 1.
 def get_wagons_1():
     wagons = []
+
+    # open file
     with open("data/trein1/wagonlijst.csv", encoding="utf-8-sig") as wagon_file:
         data = csv.reader(wagon_file, delimiter=";")
+        #skip first 10 rows, because they contain different data
         for i in range(11):
             next(data)
         for row in data:
+            # initialize variables
             w_id = row[1]
             w_type = row[2]
-            w_length = row[3]
-            w_axes = row[4]
+            w_length = float(row[3].replace(",", "."))
+            w_axes = int(row[4])
+            # create object
             wagon = Wagon(w_id, -1, w_length, -1, -1)
+            # add object to wagon list
             wagons.append(wagon)
+    # set the proper location for every wagon
+    wagons = set_location(wagons)
     return wagons
 
+# creates a list that represents the coordinates of a container.
+# it takes "##.##.##" as an input and returns [##, ##, ##]
 def get_pos(position):
     position = position.split(" ")
     position = position[0].split(".")
@@ -44,6 +61,7 @@ def get_pos(position):
     except ValueError:
         return position
 
+# function that gets all containers that are currently on the terminal (not on a loading list or something)
 def get_containers():
     container_dict = {}
     with open("data/currentContainers.csv", encoding="utf-8-sig") as containers:
@@ -51,15 +69,15 @@ def get_containers():
         next(data)
         for row in data:
             position = row[3]
-            position = position.split(".")
-            position = [int(x) for x in position]
+            position = get_pos(position)
             container_dict[row[1]] = position
     return container_dict
 
-# data not available yet
+# functions that creates fictional wagons
 def get_wagons():
     return {"a": [10,0], "b": [20,0], "c": [30,0], "d": [40,0]}
 
+# calculates the distance to the closest wagon for every container
 def calculate_distances(containers, wagons):
     distances = {}
     for key in containers.keys():
@@ -70,31 +88,34 @@ def calculate_distances(containers, wagons):
         distances[key] = min(dist_list, key= lambda t: t[1])
     return distances
 
-containers_data = get_containers()
-wagons_data = get_wagons()
-print(calculate_distances(containers_data, wagons_data))
+#containers_data = get_containers()
+#wagons_data = get_wagons()
+#print(calculate_distances(containers_data, wagons_data))
 
 # Sets the location of the wagon takes a list of all the containers in the train
 def set_location(wagons):
     len = 0
     y_val = 0
+    result = []
     for wagon in wagons:
-        if len < 320:
-            wagon.location = [math.ceil((len + 0.5 * wagon.length)/6.1), y_val]
+        if (len + wagon.length) < 320:
+            wagon.position = [math.ceil((len + 0.5 * wagon.length)/6.1), y_val]
             len += wagon.length
         else:
             len = 0
             y_val = -1
-            wagon.location = [math.ceil((len + 0.5 * wagon.length)/6/1), y_val]
+            wagon.position = [math.ceil((len + 0.5 * wagon.length)/6/1), y_val]
             len += wagon.length
+        result.append(wagon)
+    return result
 
+containers_data = get_containers()
+wagons_data = get_wagons()
+print(calculate_distances(containers_data, wagons_data))
 
-print(get_wagons_1())
+# create a list of all containers
+containers = get_containers_1()
 
-#containers = get_containers_1()
-#for container in containers:
-#    print(container.get_position())
+# create a list of all wagons
+wagons = get_wagons_1()
 
-#containers_data = get_containers()
-#wagons_data = get_wagons()
-#print(calculate_distances(containers_data, wagons_data))
