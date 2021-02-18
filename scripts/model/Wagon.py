e6e61fbcacfe27f5cb8c51bd8e1bd36480000de9
Rowin Veneman <r.veneman@student.utwente.nl>
2021-02-18 16:02:49 +0100

    TrainLoadingClasses can now run, moved all constraints to the classes

--- scripts/model/Wagon.py
+++ scripts/model/Wagon.py
@@ -27,10 +27,60 @@ class Wagon():
 
     # Constraint that a container has to be put on the wagon as a whole
     # y is the variable used in TrainLoadingX.py
-    def c_container_is_whole(self, y, num_containers):
-        for s_k, _ in enumerate(self.slots):
-            if s_k == 0:
-                return True # The first one is always fine
+    # w_j is the index of the wagon
+    # If people can optimize this, go ahead. This function will be called very often
+    def c_container_is_whole(self, y, num_containers, w_j):
+        # Create a dictionairy of all containers c_i and the slots
+        # They occupy in the wagon
+        containers = {}
+        for c_i in range(num_containers):
+            for s_k in range(len(self.slots)):
+                if(y[(c_i, w_j, s_k)] == 1):
+                    if(c_i in containers):
+                        containers[c_i].append(s_k)
+                    else:
+                        containers[c_i] = [s_k]
+        # Now check for each container if the slots they occupy are in order
+        for c_i in containers:
+            containers[c_i].sort()
+            # Since the list is ordered, the following means the container is ordered
+            return containers[c_i][-1] - containers[c_i][0] == len(containers[c_i]) - 1
+
+    def c_weight_capacity(self, y, containers, w_j):
+        # Create dictionairy of all containers c_i and their respective slots
+        containers = {}
+        for c_i in range(len(containers)):
+            for s_k in range(len(self.slots)):
+                if(y[(c_i, w_j, s_k)] == 1):
+                    if(c_i in containers):
+                        containers[c_i].append(s_k)
+                    else:
+                        containers[c_i] = [s_k]
+        # Sum the weights of all containers
+        packed_weight = 0
+        for container in containers:
+            packed_weight += container.get_net_weight()
+        return packed_weight < self.get_weight_capacity()
+    
+    def c_length_capacity(self, y, containers, w_j):
+        # Create dictionairy of all containers c_i and their respective slots
+        containers = {}
+        for c_i in range(len(containers)):
+            for s_k in range(len(self.slots)):
+                if(y[(c_i, w_j, s_k)] == 1):
+                    if(c_i in containers):
+                        containers[c_i].append(s_k)
+                    else:
+                        containers[c_i] = [s_k]
+        # count
+        packed_length = 0
+        for container in containers:
+            packed_length += container.get_length()
+        return packed_length < self.get_length_capacity()
+
+                
+
+            
             
             
         
