b458d85e07d2fcd0a8966f20e77b381d64014a20
Rowin Veneman <r.veneman@student.utwente.nl>
2021-03-17 14:40:45 +0100

    All wagon if there are enough containers

--- scripts/TrainLoadingConstraint.py
+++ scripts/TrainLoadingConstraint.py
@@ -17,6 +17,7 @@ def main(containers, train):
     # Define the cp model
     model = cp_model.CpModel()
     priority_list = []
+
     # Set some random containers to be in the priority list.
     for i in range(0, len(containers), 10):
         print("Container ", i, "Must be loaded")
@@ -27,6 +28,7 @@ def main(containers, train):
         print("Container ", i, "is hazardous")
         containers[i].set_hazard_class(1)
 
+    
 
     """
                 VARIABLES
@@ -38,6 +40,10 @@ def main(containers, train):
         for w_j, wagon in enumerate(train.wagons):
             x[(c_i,w_j)] = model.NewIntVar(0, 1, 'cont:%i,wagon%i' % (c_i, w_j))
     
+    # This value of this is: spreadContainers == (len(containers) > len(train.wagons))
+    # This is added as a constraint
+    spreadContainers = model.NewBoolVar('spreadContainers')
+
     # y = {}
     # for w_j, wagon in enumerate(train.wagons):
     #     y[(w_j)] = model.NewIntervalVar(0, wagon.get_total_length(), wagon.get_total_length(), 'wagon_slot_%i' % (w_j))
@@ -59,9 +65,19 @@ def main(containers, train):
     # Each container can be in at most one wagon.
     for c_i, container in enumerate(containers):
         if container not in priority_list:
-            model.Add(sum(x[(c_i, w_j)] for w_j, _ in enumerate(train.wagons)) <= 1)
+            model.Add(
+                sum(x[(c_i, w_j)] for w_j, _ in enumerate(train.wagons)) <= 1
+                )
 
     # Each wagon has at least one container
+    # Only enforce if there are enough containers
+    model.Add( 
+        spreadContainers == (len(containers) > len(train.wagons)) # Check if there are enough containers to spread them out over wagons
+        )
+    for w_j, _ in enumerate(train.wagons):
+        model.Add(
+            sum(x[(c_i, w_j)] for c_i, _ in enumerate(containers)) >= 1 # Each wagon has at least one container
+        ).OnlyEnforceIf(spreadContainers)
 
     # All containers in the priority list need to be loaded on the train, no matter what.
     for c_i in priority_list:
